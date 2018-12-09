64b0235bd0736e238f51eab3bdb107752f3fb7d4
Ryan BrodeFrank <ryanafrank@gmail.com>
2018-12-09 16:39:25 -0600

    added inquirer promts

--- liri.js
+++ liri.js
@@ -5,63 +5,138 @@ const fs = require("fs");
 const moment = require("moment");
 const axios = require("axios");
 const Spotify = require("node-spotify-api");
-
+const inquirer = require("inquirer");
 const keys = require("./keys");
 const spotify = new Spotify(keys.spotify);
 
 // console.log(keys);
 
-var userChoice = process.argv[2];
-var input = process.argv[3];
-for (var i = 4; i < process.argv.length; i++) {
-    input += " " + process.argv[i];
+// Using process.argv to get user input
+// var userChoice = process.argv[2];
+// var input = process.argv[3];
+// for (var i = 4; i < process.argv.length; i++) {
+//     input += " " + process.argv[i];
+// }
+
+
+// User input via inquirer
+var input = "";
+var limit = 0;
+function callType() {
+    inquirer.prompt([
+        {
+            name: "call",
+            type: "list",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says", "exit"],
+            message: "What type of call do you want to use?"
+        }
+    ]).then(function (response) {
+        switch (response.call) {
+            case "concert-this":
+                inquirer.prompt([
+                    {
+                        name: "input",
+                        type: "input",
+                        message: "What artist to you want to search for upcoming concerts?"
+                    }
+                ]).then(function (search) {
+                    input = search.input;
+                    console.log("\nBandsInTown");
+                    concertThis();
+                });
+
+                break;
+            case "spotify-this-song":
+                inquirer.prompt([
+                    {
+                        name: "input",
+                        type: "input",
+                        message: "What song do you want information about?"
+                    },
+                    {
+                        name: "limit",
+                        type: "input",
+                        message: "How many songs do you want returned?s"
+                    }
+                ]).then(function (search) {
+                    input = search.input;
+
+                    if (!Number.isNaN(parseInt(search.limit))) {
+                        limit = search.limit;
+                        console.log("\nSpotify");
+                        spotifyThisSong();
+                    } else {
+                        console.log(`"${search.limit}" is not a valid number input`);
+                        numberInput();
+                    }
+
+                });
+
+                break;
+            case "movie-this":
+                inquirer.prompt([
+                    {
+                        name: "input",
+                        type: "input",
+                        message: "What movie do you want information about?"
+                    }
+                ]).then(function (search) {
+                    input = search.input;
+                    console.log("\nMovie");
+                    movieThis();
+                });
+                break;
+            case "do-what-it-says":
+                console.log("\nWhatever");
+                fs.readFile("./random.txt", "utf8", function (error, data) {
+                    if (error) {
+                        throw error;
+                    }
+                    // console.log(data);
+                    var arr = data.split("::");
+                    // console.log(arr);
+                    var rand = Math.floor(Math.random() * arr.length);
+                    var pick = arr[rand].split(", ");
+                    // console.log(pick);
+                    input = pick[1].replace(/"/g, "");
+                    // console.log(input);
+                    if (pick[0] === "concert-this") {
+                        concertThis();
+                    } else if (pick[0] === "spotify-this-song") {
+                        spotifyThisSong();
+                    } else if (pick[0] === "movie-this") {
+                        movieThis();
+                    }
+                });
+                break;
+            default:
+                console.log("Goodbye.");
+                break;
+        }
+    });
 }
 
-switch (userChoice) {
-    case "concert-this":
-        console.log("\nBandsInTown");
-        concertThis();
-        break;
-    case "spotify-this-song":
-        spotifyThisSong();
-        console.log("\nSpotify");
-        break;
-    case "movie-this":
-        console.log("\nMovie");
-        movieThis();
-        break;
-    case "do-what-it-says":
-        console.log("\nWhatever");
-        fs.readFile("./random.txt", "utf8", function (error, data) {
-            if (error) {
-                throw error;
-            }
-            // console.log(data);
-            var arr = data.split("::");
-            // console.log(arr);
-            var rand = Math.floor(Math.random() * arr.length);
-            var pick = arr[rand].split(", ");
-            // console.log(pick);
-            input = pick[1].replace(/"/g, "");
-            // console.log(input);
-            if (pick[0] === "concert-this") {
-                concertThis();
-            } else if (pick[0] === "spotify-this-song") {
-                spotifyThisSong();
-            } else if (pick[0] === "movie-this") {
-                movieThis();
-            }
-        });
-        break;
-    default:
-        console.log("ERROR: Invalid Input");
-        console.log("Valid Inputs: concert-this, spotify-this-song, movie-this, do-what-it-says");
-        break;
-}
+function numberInput() {
+    inquirer.prompt([
+        {
+            name: "limit",
+            type: "input",
+            message: "How many songs do you want returned?s"
+        }
+    ]).then(function (search) {
+        if (!Number.isNaN(parseInt(search.limit))) {
+            limit = search.limit;
+            console.log("\nSpotify");
+            spotifyThisSong();
+        } else {
+            console.log(`"${search.limit}" is not a valid number input`);
+            numberInput();
+        }
 
+    });
+}
 
 function concertThis() {
-
     console.log("Requested Artist: " + input);
     // console.log("key: " + keys.bands.key);
     var urlQuery = `https://rest.bandsintown.com/artists/${input}/events?app_id=${keys.bands.key}`
@@ -72,11 +147,11 @@ function concertThis() {
             console.log(`${response.data[i].venue.city}, ${response.data[i].venue.country}`);
             console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
         }
+        callType();
     });
 }
 
 function spotifyThisSong() {
-
     // console.log("id: " + keys.spotify.id);
     // console.log("key: " + keys.spotify.secret);
     if (input == undefined) {
@@ -86,6 +161,7 @@ function spotifyThisSong() {
             console.log(`Title: ${data.name}`); //Album Name
             console.log(data.external_urls.spotify);//Spotify Link
             console.log(`Album: ${data.album.name}`); //Album Name
+            callType();
         }).catch(function (error) {
             console.error('Error occurred: ' + error);
         });
@@ -93,7 +169,7 @@ function spotifyThisSong() {
         spotify.search({
             type: "track",
             query: input,
-            limit: 1,
+            limit: limit,
         }, function (error, response) {
             if (error) {
                 return console.log(`Error occurred: ${error}`);
@@ -110,13 +186,12 @@ function spotifyThisSong() {
                 console.log(song.external_urls.spotify);//Spotify Link
                 console.log(`Album: ${song.album.name}`); //Album Name
             }
-
+            callType();
         });
     }
 }
 
 function movieThis() {
-
     // console.log(input);
     if (input == undefined) {
         input = "Mr. Nobody";
@@ -130,5 +205,8 @@ function movieThis() {
         console.log(`Languages: ${response.data.Language}`); //Movie Languages
         console.log(`Plot: ${response.data.Plot}`); //Movie Plots
         console.log(`Actors: ${response.data.Actors}`); //Actors List
+        callType();
     });
 }
+
+callType();
